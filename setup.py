6fe4793b470d870de916789036a06822bf2fadd1
Krzysztof Szumny <noisy.pl@gmail.com>
2018-10-25 07:20:55 +0200

    changes compatible with pkg_resources.resource_filename

--- setup.py
+++ setup.py
@@ -24,11 +24,7 @@ setup(name='eosfactory',
     author_email='contact@tokenika.io',
     license='MIT',
     packages=find_packages(),
-    # include_package_data=True,
-    data_files=[
-        ('ln', ['localnode/config.ini', 'localnode/genesis.json']),
-        ('/opt/ef', ['resources/tokenika.gif']),
-    ],
+    include_package_data=True,
     install_requires=[
         'termcolor',
     ],
